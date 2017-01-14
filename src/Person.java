e208d1705824652faac574dee562d3d9bf95adf3
Eriks <eriks.labsirdiba@outlook.com>
2017-01-14 14:35:44 +0200

    Makes weight private and gives it a setter and a getter

--- src/Person.java
+++ src/Person.java
@@ -2,24 +2,33 @@
 public class Person {
 	
 	float height;
-	float weight;
+	private double weight;
 	
+	//get weight
+	public double getWeight() {
+		return weight;
+	}
+	
+	//set weight
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
 
 	//constructor
 	public Person(){
 		this.height=6;
-		this.weight = 200;
+		this.setWeight(200);
 	}
 	
 	public void walks(){
-		this.weight *= 0.9;
+		this.setWeight(this.getWeight() * 0.9);
 		System.out.println("You lost weight!");
-		System.out.println("You weigh: "+ this.weight+"kg");
+		System.out.println("You weigh: "+ this.getWeight()+"kg");
 	}
 	
 	public void eats(){
-		this.weight *=1.2;
+		this.setWeight(this.getWeight() * 1.2);
 		System.out.println("You gained weight!");
-		System.out.println("You weigh: "+this.weight+"kg");
+		System.out.println("You weigh: "+this.getWeight()+"kg");
 	}
 }
--- src/Player.java
+++ src/Player.java
@@ -2,7 +2,9 @@
 public class Player {
 	
 	public static void main(String[] args) {
+		int x =100;
 		Person tom = new Person();
+		tom.setWeight(x);
 for(int i=0;i<=10;i++){
 	if(i%2==0){
 		tom.eats();
@@ -10,7 +12,7 @@ for(int i=0;i<=10;i++){
 		tom.walks();
 	}
 }
-
+	
 
 	}
 
